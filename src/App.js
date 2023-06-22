a11a4ff60768438e49d0cf50d542b2d36768b90d
SlavTheKoder <valkrukov@gmail.com>
2023-06-23 00:21:57 +0300

    1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className="app-wrapper">
+      <header className="header">
+        <img
+          src="https://qahwaworld.com/wp-content/uploads/2022/01/coffee-quality-institute.jpg"
+          className="logo"
+        ></img>
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"></img>
+        </div>
+        <div>ava + description</div>
+        <div>
+          my posts
+          <div>new post</div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
     </div>
   );
 };
