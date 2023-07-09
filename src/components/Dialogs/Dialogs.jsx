ccd93d5395e65d5fc63b8f7e228bd83ab10eecac
SlavTheKoder <valkrukov@gmail.com>
2023-07-09 16:17:35 +0300

    create ProfileInfo component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,38 @@
+import {NavLink} from 'react-router-dom';
 import s from './Dialogs.module.css';
 
+const DialogItem = (props) => {
+  let path = '/dialogs/' + props.id;
+  return (
+    <div className={s.dialog + '' + s.active}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  return <div className={s.dialog}>{props.message}</div>;
+};
+
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <DialogItem name="Valeriy" id="1" />
+        <DialogItem name="EvilArthas" id="2" />
+        <DialogItem name="TorontoTokyo" id="3" />
+        <DialogItem name="BadComedian" id="4" />
+        <DialogItem name="Solo from 9Pandas" id="4" />
+        <DialogItem name="Ramzes 666" id="5" />
+        <DialogItem name="Gabe Neuwell" id="6" />
+      </div>
+      <div className={s.messages}>
+        <Message message="This is fine..." />
+        <Message message="KK" />
+        <Message message="qq how are you?" />
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,16 @@
+import s from './ProfileInfo.module.css';
+const ProfileInfo = () => {
+  return (
+    <div>
+      <ProfileInfo />
+      <div>
+        <img
+          src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          className={s.logo}
+        ></img>
+      </div>
+      <div>ava + description</div>
+    </div>
+  );
+};
+export default ProfileInfo;
