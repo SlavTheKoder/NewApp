33cad8d5c366dd519aef1e786be5134f6de5fee3
SlavTheKoder <valkrukov@gmail.com>
2023-07-09 14:08:32 +0300

    create active link style

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,23 +1,61 @@
 import s from './Navbar.module.css';
 import {NavLink} from 'react-router-dom';
+// import activeLink from './selected';
+
+const setActive = ({isActive}) => (isActive ? 'active-link' : '');
 
 const NavBar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <NavLink to="/profile">Profile</NavLink>
+        <NavLink
+          to="/profile"
+          style={({isActive}) => ({
+            color: isActive ? 'var(--color-active' : 'white',
+          })}
+        >
+          Profile
+        </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="news">News</NavLink>
+        <NavLink
+          to="news"
+          style={({isActive}) => ({
+            color: isActive ? 'var(--color-active' : 'white',
+          })}
+        >
+          News
+        </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="">Music</NavLink>
+        <NavLink
+          to="music"
+          style={({isActive}) => ({
+            color: isActive ? 'var(--color-active' : 'white',
+          })}
+        >
+          Music
+        </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="dialogs">Messages</NavLink>
+        <NavLink
+          to="dialogs"
+          style={({isActive}) => ({
+            color: isActive ? 'var(--color-active' : 'white',
+          })}
+        >
+          Messages
+        </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to>Settings</NavLink>
+        <NavLink
+          to="settings"
+          style={({isActive}) => ({
+            color: isActive ? 'var(--color-active' : 'white',
+          })}
+        >
+          Settings
+        </NavLink>
       </div>
     </nav>
   );
