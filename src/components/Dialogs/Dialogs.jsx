1543e8af6e844706845c4c3cc498aeb7bfb04009
SlavTheKoder <valkrukov@gmail.com>
2023-07-15 01:00:46 +0300

    rework components in Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -15,7 +15,7 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
-  let dialogsData = [
+  let dialogs = [
     {id: 1, name: 'Valeriy'},
     {id: 2, name: 'EvilArthas'},
     {id: 3, name: 'TorontoTokyo'},
@@ -24,27 +24,23 @@ const Dialogs = (props) => {
     {id: 6, name: 'Ramzes 666'},
     {id: 7, name: 'Gabe Neuwell'},
   ];
-  let messagesData = [
+  let messages = [
     {id: 1, message: 'This is fine...'},
     {id: 2, message: 'KK'},
     {id: 3, message: 'qq how are you?'},
   ];
+
+  let dialogElements = dialogs.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+
+  let messagesElements = messages.map((m) => (
+    <Message message={m.message} id={m.id} />
+  ));
   return (
     <div className={s.dialogs}>
-      <div className={s.dialogsItems}>
-        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-        <DialogItem name="TorontoTokyo" id="3" />
-        <DialogItem name="BadComedian" id="4" />
-        <DialogItem name="Solo from 9Pandas" id="5" />
-        <DialogItem name="Ramzes 666" id="6" />
-        <DialogItem name="Gabe Neuwell" id="7" />
-      </div>
-      <div className={s.messages}>
-        <Message message={messagesData[0].message} id={dialogsData[0].id} />
-        <Message message={messagesData[1].message} id={dialogsData[1].id} />
-        <Message message={messagesData[2].message} id={dialogsData[2].id} />
-      </div>
+      <div className={s.dialogsItems}>{dialogElements}</div>
+      <div className={s.messages}>{messagesElements}</div>
     </div>
   );
 };
