23be857e152ba92b3898eac928ed0336e1d74ae1
SlavTheKoder <valkrukov@gmail.com>
2023-07-15 01:03:34 +0300

    rework components in MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,10 +1,14 @@
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 const MyPosts = () => {
-  let postsData = [
+  let posts = [
     {id: 1, message: 'Hello World!', likesCount: 12},
     {id: 2, message: 'One More Disaster Artist', likesCount: 13},
   ];
+
+  let postsElemenets = posts.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
@@ -15,16 +19,7 @@ const MyPosts = () => {
         <div>
           <button>Add Post</button>
         </div>
-        <div className={s.posts}>
-          <Post
-            message={postsData[0].message}
-            likesCount={postsData[0].likesCount}
-          />
-          <Post
-            message={postsData[1].message}
-            likesCount={postsData[1].likesCount}
-          />
-        </div>
+        <div className={s.posts}>{postsElemenets}</div>
       </div>
     </div>
   );
